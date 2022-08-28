8298fdda5c086b982b7be76da6a46aae529f02ec
msmel01 <ms.elly17.01@gmail.com>
2022-08-27 23:08:00 -0400

    fixed bufg where navigation links covered other dom elements

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -65,15 +65,6 @@ window.addEventListener("wheel", () => userScrolling = true);
 window.addEventListener("touchmove", () => userScrolling = true);
 window.addEventListener("mousedown", () => userScrolling = true);
 
-// for clickable event
-timelineNav.forEach(v => {
-	v.onclick = (() => {
-		userScrolling = false;
-		timelineNav.forEach(j=> j.classList.remove('active'));
-		v.classList.add('active');
-	 });
-});
-
 var above = [];
 
 window.onscroll = (()=> {
@@ -106,93 +97,12 @@ window.onscroll = (()=> {
 	}
 
 });
-// const observer = new IntersectionObserver(entries => {
-// 	entries.forEach(entry => {
-// 	  const exp = entry.target;
-  
-// 	  if (exp.isIntersecting) {
-// 		exp.classList.add('experience-animation');
-// 		return; // if we added the class, exit the function
-// 	  }
-  
-// 	  // We're not intersecting, so remove the class!
-// 	  exp.classList.remove('experience-animation');
-// 	});
-//   });
 
-// document.querySelectorAll('.experience').forEach(div => {
-// 	observer.observe(div);
-// });
-
-// $(() => {
-// 	let stickyTop = 0,
-// 	scrollTarget = false;
-  
-// 	let timeline = $('.timeline__nav'),
-// 	items = $('li', timeline),
-// 	milestones = $('.timeline__section .milestone'),
-// 	offsetTop = parseInt(timeline.css('top'));
-  
-// 	const TIMELINE_VALUES = {
-// 	  start: 190,
-// 	  step: 30 };
-  
-  
-// 	$(window).resize(function () {
-// 	  timeline.removeClass('fixed');
-  
-// 	  stickyTop = timeline.offset().top - offsetTop;
-  
-// 	  $(window).trigger('scroll');
-// 	}).trigger('resize');
-  
-// 	$(window).scroll(function () {
-// 	  if ($(window).scrollTop() > stickyTop) {
-// 		timeline.addClass('fixed');
-// 	  } else {
-// 		timeline.removeClass('fixed');
-// 	  }
-// 	}).trigger('scroll');
-  
-// 	items.find('span').click(function () {
-// 	  let li = $(this).parent(),
-// 	  index = li.index(),
-// 	  milestone = milestones.eq(index);
-  
-// 	  if (!li.hasClass('active') && milestone.length) {
-// 		scrollTarget = index;
-  
-// 		let scrollTargetTop = milestone.offset().top - 80;
-  
-// 		$('html, body').animate({ scrollTop: scrollTargetTop }, {
-// 		  duration: 400,
-// 		  complete: function complete() {
-// 			scrollTarget = false;
-// 		  } });
-  
-// 	  }
-// 	});
-  
-// 	$(window).scroll(function () {
-// 	  let viewLine = $(window).scrollTop() + $(window).height() / 3,
-// 	  active = -1;
-  
-// 	  if (scrollTarget === false) {
-// 		milestones.each(function () {
-// 		  if ($(this).offset().top - viewLine > 0) {
-// 			return false;
-// 		  }
-  
-// 		  active++;
-// 		});
-// 	  } else {
-// 		active = scrollTarget;
-// 	  }
-  
-// 	  timeline.css('top', -1 * active * TIMELINE_VALUES.step + TIMELINE_VALUES.start + 'px');
-  
-// 	  items.filter('.active').removeClass('active');
-  
-// 	  items.eq(active != -1 ? active : 0).addClass('active');
-// 	}).trigger('scroll');
-//   });
+// for clickable event
+timelineNav.forEach(v => {
+	v.onclick = (() => {
+		userScrolling = false;
+		timelineNav.forEach(j=> j.classList.remove('active'));
+		v.classList.add('active');
+	 });
+});
